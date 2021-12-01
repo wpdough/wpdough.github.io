d041d46b0e124c55dd0a9ae978ac2653253a922f
wpdough <me@wpdough.com>
2021-12-01 15:31:53 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // purge: {
+  //   enabled: true,
+  //   content: ["./src/*.html"],
+  // },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Outfit']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
